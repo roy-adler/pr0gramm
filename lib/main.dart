a135341d343e036f66aca9a2ea527a53100d2830
Adler <royadler@outlook.com>
2019-07-24 09:33:49 +0200

    Commitit

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,76 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
-void main() => runApp(Pr0gramm());
+void main() => runApp(MyApp());
 
-class Pr0gramm extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Node server demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Pr0gramm'),
-        ),
-        body: Center(child: Text('Hello Schwuchti')),
+        appBar: AppBar(title: Text('Flutter Client')),
+        body: BodyWidget(),
       ),
     );
+  }
+}
+
+class BodyWidget extends StatefulWidget {
+  @override
+  BodyWidgetState createState() {
+    return new BodyWidgetState();
+  }
+}
+
+class BodyWidgetState extends State<BodyWidget> {
+  String serverResponse = 'Server response';
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: Align(
+        alignment: Alignment.topCenter,
+        child: SizedBox(
+          width: 200,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                child: Text('Send request to server'),
+                onPressed: () {
+                  _makeGetRequest();
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(serverResponse),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _makeGetRequest() async {
+    Response response = await get(_localhost());
+    setState(() {
+      serverResponse = response.body;
+    });
+  }
+
+  String _localhost() {
+    if (Platform.isAndroid)
+      return 'http://10.0.2.2:3000';
+    else // for iOS simulator
+      return 'http://localhost:3000';
   }
 }
