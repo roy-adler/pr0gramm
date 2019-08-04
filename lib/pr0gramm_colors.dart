dcb9e3f6f5e196777a117c967819790cb4f868e5
Adler <royadler@outlook.com>
2019-08-04 14:33:03 +0200

    Added Pr0grammColors and Pr0Text

---
+++ lib/pr0gramm_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+final Color richtigesGrau = const Color(0xff161618);
+final Color pr0grammOrange = const Color(0xffee4d2e);
+final Color standardSchriftfarbe = const Color(0xfff2f5f4);
+final Color standardSchriftfarbeAusgegraut = const Color(0xff888888);
+final Color linkfarbe = const Color(0xff75c0c7);
+final Color IRGENDWASDOOFESISTPASSIERTFarbe = const Color(0xfffc8833);
+final Color cha0sGrau = const Color(0xff212121);
+final Color pr0miumPostGelb = const Color(0xfff7c516);
+final Color SFWGruen = const Color(0xff5cb85c);
+final Color NSFWOrange = const Color(0xfff0ad4e);
+final Color NSFLRot = const Color(0xffd9534f);
+final Color pr0miumAuswahlteile = const Color(0xff222222);
+final Color ehemaligeHintergrundFarbeDerKommentare = const Color(0xff2a2e31);
+final Color altesPr0gramm = const Color(0xffff0082);
