9983e5a2e9fcd7682176dd86b079dfcacaf6f6a9
Roy Adler <royadler@outlook.com>
2019-08-20 12:20:37 +0200

    Why are we still here?

--- lib/pages/video_screen.dart
+++ lib/pages/video_screen.dart
@@ -37,5 +37,23 @@ class _VideoScreenState extends State<VideoScreen> {
   @override
   Widget build(BuildContext context) {
     // Complete the code in the next step.
+    return FutureBuilder(
+      future: _initializeVideoPlayerFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          // If the VideoPlayerController has finished initialization, use
+          // the data it provides to limit the aspect ratio of the VideoPlayer.
+          return AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            // Use the VideoPlayer widget to display the video.
+            child: VideoPlayer(_controller),
+          );
+        } else {
+          // If the VideoPlayerController is still initializing, show a
+          // loading spinner.
+          return Center(child: CircularProgressIndicator());
+        }
+      },
+    );
   }
 }
