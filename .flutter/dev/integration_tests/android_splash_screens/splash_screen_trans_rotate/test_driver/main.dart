892fd71a4b714639d5d88ea510acbe5b29d7eb8f
roy-adler <royadler@outlook.com>
2019-10-31 16:25:42 +0100

    Revert "adding flutterw"
    This reverts commit 8035b2102d9371a481bf7d8020de73bb876aefa8.

--- .flutter/dev/integration_tests/android_splash_screens/splash_screen_trans_rotate/test_driver/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_driver/driver_extension.dart';
-import 'package:splash_screen_trans_rotate/main.dart' as app;
-
-void main() {
-  enableFlutterDriverExtension();
-  app.main();
-}
