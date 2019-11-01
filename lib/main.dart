731fbb84ddb24a4bc39b5e1a6cfa216285e41e6b
roy-adler <royadler@outlook.com>
2019-11-01 23:15:43 +0100

    updated main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pr0gramm_app/api/preferences.dart';
+import 'package:pr0gramm_app/api/response_parser.dart';
+import 'package:pr0gramm_app/content/is_loggedIn.dart';
+import 'package:pr0gramm_app/design/pr0gramm_colors.dart';
+import 'package:pr0gramm_app/pages/main_page.dart';
 
 import 'pages/login_page.dart';
 
@@ -9,7 +14,45 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
-      home: LoginPage(),
+      home: LoadStart(),
+    );
+  }
+}
+
+class LoadStart extends StatelessWidget {
+  _loadLastLogin() async {
+    String cookies = await Preferences.cookies();
+    if (cookies != null) {
+      await ResponseParser.setCookie(cookies);
+      IsLoggedIn isLoggedIn = await ResponseParser.isLoggedIn();
+      if (isLoggedIn.loggedIn) {
+        return true;
+      }
+      return false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      backgroundColor: richtigesGrau,
+      child: FutureBuilder(
+        future: _loadLastLogin(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            if (snapshot.data) {
+              return MainPage();
+            } else {
+              return LoginPage();
+            }
+          }
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: pr0grammOrange,
+            ),
+          );
+        },
+      ),
     );
   }
 }
