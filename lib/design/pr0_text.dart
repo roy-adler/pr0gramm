64e516516d1e3f57e2706be293ed86a05c8c1af2
Roy Adler <royadler@outlook.com>
2019-11-08 17:56:27 +0100

    Pr0text extended

--- lib/design/pr0_text.dart
+++ lib/design/pr0_text.dart
@@ -4,15 +4,20 @@ import 'package:pr0gramm_app/design/pr0gramm_colors.dart';
 class Pr0Text extends StatelessWidget {
   final String text;
   final TextAlign textAlign;
+  final bool heading;
 
-  const Pr0Text(this.text, {this.textAlign = TextAlign.center});
+  const Pr0Text(this.text,
+      {this.textAlign = TextAlign.center, this.heading = false});
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
       textAlign: textAlign,
-      style: TextStyle(color: standardSchriftfarbe),
+      style: TextStyle(
+        color: standardSchriftfarbe,
+        fontSize: heading ? 22 : 16,
+      ),
     );
   }
 }
