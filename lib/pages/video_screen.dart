c231689cd8e590aa52ed8a1a12136fb532a978af
Roy Adler <royadler@outlook.com>
2020-01-24 17:50:51 +0100

    updating flutter and flutter plugins

--- lib/pages/video_screen.dart
+++ lib/pages/video_screen.dart
@@ -14,7 +14,7 @@ class _VideoScreenState extends State<VideoScreen> {
   void initState() {
     super.initState();
     _controller = VideoPlayerController.network(
-        "http://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4")
+        'http://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4')
       ..initialize().then((_) {
         // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
         setState(() {});
@@ -23,27 +23,27 @@ class _VideoScreenState extends State<VideoScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        _controller.value.initialized
+    return Scaffold(
+      body: Center(
+        child: _controller.value.initialized
             ? AspectRatio(
                 aspectRatio: _controller.value.aspectRatio,
                 child: VideoPlayer(_controller),
               )
             : Container(),
-        CupertinoButton(
-          onPressed: () {
-            setState(() {
-              _controller.value.isPlaying
-                  ? _controller.pause()
-                  : _controller.play();
-            });
-          },
-          child: Icon(
-            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-          ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _controller.value.isPlaying
+                ? _controller.pause()
+                : _controller.play();
+          });
+        },
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
         ),
-      ],
+      ),
     );
   }
 
