a71db0141a2eedb75471a33e015792df8b1a065a
Roy Adler <royadler@outlook.com>
2020-01-26 22:49:24 +0100

    TagPage updated

--- lib/pages/tag_page.dart
+++ lib/pages/tag_page.dart
@@ -1,13 +1,46 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:pr0gramm/content/pr0gramm_tag.dart';
 
-class TagPage extends StatelessWidget {
+class TagPage extends StatefulWidget {
   final List<Pr0grammTag> tagList;
 
   TagPage({@required this.tagList});
 
   @override
+  _TagPageState createState() => _TagPageState();
+}
+
+class _TagPageState extends State<TagPage> {
+  bool folded;
+  int maxFoldedItems;
+
+  @override
+  void initState() {
+    folded = true;
+    maxFoldedItems = min(4, widget.tagList.length);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Wrap(children: tagList.sublist(0, 2));
+    return Column(
+      children: <Widget>[
+        AnimatedContainer(
+          color: Colors.red,
+          duration: Duration(milliseconds: 500),
+          child: folded
+              ? Wrap(children: widget.tagList.sublist(0, maxFoldedItems))
+              : Wrap(children: widget.tagList),
+        ),
+        CupertinoButton(
+          child: Icon(
+              folded ? CupertinoIcons.down_arrow : CupertinoIcons.up_arrow),
+          onPressed: () => setState(() => folded = !folded),
+        )
+      ],
+    );
   }
 }
