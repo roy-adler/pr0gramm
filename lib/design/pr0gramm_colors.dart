643b0d7f4125fcdb226e919d770428d50609de51
Roy Adler <royadler@outlook.com>
2020-06-03 23:18:12 +0200

    Pr0grammColors are constant now

--- lib/design/pr0gramm_colors.dart
+++ lib/design/pr0gramm_colors.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 
-final Color richtigesGrau = const Color(0xff161618);
-final Color pr0grammOrange = const Color(0xffee4d2e);
-final Color standardSchriftfarbe = const Color(0xfff2f5f4);
-final Color standardSchriftfarbeAusgegraut = const Color(0xff888888);
-final Color linkfarbe = const Color(0xff75c0c7);
-//irgendwasdoofesistpassiertfarbe
-final Color iRGENDWASDOOFESISTPASSIERTFarbe = const Color(0xfffc8833);
-final Color cha0sGrau = const Color(0xff212121);
-final Color pr0miumPostGelb = const Color(0xfff7c516);
-final Color sFWGruen = const Color(0xff5cb85c);
-final Color nSFWOrange = const Color(0xfff0ad4e);
-final Color nSFLRot = const Color(0xffd9534f);
-final Color pr0miumAuswahlteile = const Color(0xff222222);
-final Color ehemaligeHintergrundFarbeDerKommentare = const Color(0xff2a2e31);
-final Color altesPr0gramm = const Color(0xffff0082);
-final Color tagHintergrund = const Color(0xa1888888);
+const Color richtigesGrau = const Color(0xff161618);
+const Color pr0grammOrange = const Color(0xffee4d2e);
+const Color standardSchriftfarbe = const Color(0xfff2f5f4);
+const Color standardSchriftfarbeAusgegraut = const Color(0xff888888);
+const Color linkfarbe = const Color(0xff75c0c7);
+const Color iRGENDWASDOOFESISTPASSIERTFarbe = const Color(0xfffc8833);
+const Color cha0sGrau = const Color(0xff212121);
+const Color pr0miumPostGelb = const Color(0xfff7c516);
+const Color sFWGruen = const Color(0xff5cb85c);
+const Color nSFWOrange = const Color(0xfff0ad4e);
+const Color nSFLRot = const Color(0xffd9534f);
+const Color pr0miumAuswahlteile = const Color(0xff222222);
+const Color ehemaligeHintergrundFarbeDerKommentare = const Color(0xff2a2e31);
+const Color altesPr0gramm = const Color(0xffff0082);
+const Color tagHintergrund = const Color(0xa1888888);
